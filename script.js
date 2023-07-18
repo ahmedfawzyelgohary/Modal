b096879792d32d44d36c5a222c3ef611878c0092
elgohary <ahmedfawyelgohary@yahoo.com>
2023-07-18 14:27:11 +0300

    learn some new skills and make new project

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModal);
+}
+// make function called closeModal and replace it in the code and do the same thing in function openModal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
